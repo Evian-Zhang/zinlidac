53e0601b160c8ddeb66d1abbce08e7dbee2f1ab3
Evian-Zhang <evianzhang1999@163.com>
2020-06-02 10:06:09 +0800

    delete posix detection because it is derived at compile time

--- lib/system/system.h
+++ lib/system/system.h
@@ -1,12 +1,4 @@
 namespace libzinlidac {
-// --------------------------- POSIX ---------------------------
-// get the version number of POSIX.1 that the library and kernel support
-// return -1 both if the implementation does not impose a limit, and in case of an error
-unsigned long get_posix1_version();
-// get the version number of POSIX.2 that the system utilities support
-// return -1 both if the implementation does not impose a limit, and in case of an error
-unsigned long get_posix2_version();
-
 // --------------------------- Filesystem ---------------------------
 // get max length of file path
 // return -1 both if no limit for path length
