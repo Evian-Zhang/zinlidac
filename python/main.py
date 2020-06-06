94fc41f74d9f3058a1296a3b8790ef355f56a4b9
Evian-Zhang <evianzhang1999@163.com>
2020-06-06 16:47:14 +0800

    complete arg parser and json output

--- python/main.py
+++ python/main.py
@@ -1,58 +1,48 @@
+from enum import Enum
+import json
+
 from graphene import Schema
+from graphql import GraphQLError
+from graphql.error import format_error as format_graphql_error
+import argparse
 
 import schema.schema as schema_module
-import libzinlidacpy
+
+class Format(Enum):
+    JSON = 1
+    ProtoBuf = 2
+
+# see https://github.com/graphql-python/graphene-django/blob/master/graphene_django
+def format_error(error):
+    if isinstance(error, GraphQLError):
+        return format_graphql_error(error)
+    else:
+        return {'message': str(error)}
 
 if __name__ == '__main__':
-    zinlidac_schema = Schema(query=schema_module.Query, auto_camelcase=False)
-    query = """{ 
-        hardware { 
-            cpu { hardware_type } 
-            cache { level4_cache_size }
-            memory { available_physical_memory_pages_number }
-        }
-        system {
-            operating_system { system_name }
-            filesystem {
-                filesystem_capacity
-                current_mounted_filesystems {
-                    name
-                    dir
-                    mount_type
-                    mount_options
-                }
-            }
-            user {
-                associated_user {
-                    name
-                    uid
-                }
-                users {
-                    name
-                    login_shell
-                }
-                groups {
-                    name
-                }
-            }
-            process {
-                processes {
-                    cmdline_arguments,
-                    basename
-                }
-            }
-            network {
-                network_interfaces {
-                    name
-                    address
-                }
-                lsof_i {
-                    command
-                    user
-                    fd
-                }
-            }
-        }
-    }"""
-    print(zinlidac_schema.execute(query))
-    
+    parser = argparse.ArgumentParser(description="Python wrapper for libzinlidac")
+    parser.add_argument("graphql_query", help="path of input GraphQL query file")
+    parser.add_argument("-f", "--format", nargs='?', default='json', help="format of output results")
+    parser.add_argument("-o", "--output", nargs='?', default='results.json', help="path of output results file")
+
+    args = parser.parse_args()
+    if args.format == "json":
+        format = Format.JSON
+    elif args.format == "protobuf":
+        format = Format.ProtoBuf
+    else:
+        raise Exception("Unknown format type")
+
+    with open(args.graphql_query, 'r') as query_file:
+        query = query_file.read()
+        zinlidac_schema = Schema(query=schema_module.Query, auto_camelcase=False)
+        query_results = zinlidac_schema.execute(query)
+        results = {'data': query_results.data}
+        if query_results.errors:
+            results['errors'] = [format_error(e) for e in query_results.errors]
+        if format == Format.JSON:
+            results_str = json.dumps(results, ensure_ascii=False, indent=4)
+        elif format == Format.ProtoBuf:
+            results_str = ""
+        with open(args.output, 'w+', encoding="utf-8") as results_file:
+            results_file.write(results_str)
